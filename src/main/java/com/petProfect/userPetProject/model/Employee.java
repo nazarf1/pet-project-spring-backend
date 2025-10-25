2c542df0a2a32facec414d6213aa76331518ec37
nazarf1 <nazarfedof1@gmail.com>
2025-10-25 18:57:44 -0400

    implement uuid for id

--- src/main/java/com/petProfect/userPetProject/model/Employee.java
+++ src/main/java/com/petProfect/userPetProject/model/Employee.java
@@ -3,13 +3,16 @@ package com.petProfect.userPetProject.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @Entity
 @Table(name = "Employees")
 public class Employee {
     @Id
     @GeneratedValue
-    private long id;
+    @Column(updatable = false, nullable = false)
+    private UUID id;
     private String firstName;
     private String lastName;
     @Column(unique = true)
